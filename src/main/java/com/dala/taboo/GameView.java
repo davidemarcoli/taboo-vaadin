882010a4cb1fcff7c160c7d40451ec6679d515a3
lazarpe <lazar.petrovic.nefti@gmail.com>
2022-01-25 14:04:23 +0100

    Timer added with thread (with help from Davide :D)

--- src/main/java/com/dala/taboo/GameView.java
+++ src/main/java/com/dala/taboo/GameView.java
@@ -3,21 +3,27 @@ package com.dala.taboo;
 import com.vaadin.flow.component.UI;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.html.H2;
+import com.vaadin.flow.component.html.H3;
 import com.vaadin.flow.component.html.ListItem;
 import com.vaadin.flow.component.html.UnorderedList;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.page.Push;
 import com.vaadin.flow.router.Route;
 
 import java.util.ArrayList;
+import java.util.Timer;
 
+@Push
 @Route(value = "game")
 public class GameView extends VerticalLayout {
-
+    static H3 timeLeft = new H3();
     TabooWord currentWord;
     Button nextButton = new Button();
 
     H2 word = new H2();
     UnorderedList tabooWords = new UnorderedList();
+
+    Thread timer;
 
     public GameView() {
         if (DataService.files.size() < 1) {
@@ -26,15 +32,42 @@ public class GameView extends VerticalLayout {
             return;
         }
 
+//        countDown(10);
+
         newWord();
 
         nextButton.setText("Next Word!");
 
         nextButton.addClickListener(event -> {
             newWord();
+            startTimer();
         });
 
-        add(word, tabooWords, nextButton);
+        add(word, tabooWords, nextButton, timeLeft);
+
+        System.out.println("It should now all be added!");
+    }
+
+    public void startTimer() {
+        try {
+            timer.interrupt();
+        } catch (Exception ignore) { }
+        timer = new Thread(new TimeManagement(12, timeLeft, getUI().orElse(null), this));
+        timer.start();
+
+//        synchronized (timer) {
+//            System.out.println("Timer finished");
+//            try {
+//                timer.wait();
+//                System.out.println("The Timer wait has finished!");
+//            } catch (InterruptedException e) {
+//                e.printStackTrace();
+//            }
+//        }
+
+       // System.out.println("Wait finished!");
+
+    //    timeLeft.setText("Time is up!!!");
     }
 
     public void newWord() {
@@ -49,6 +82,5 @@ public class GameView extends VerticalLayout {
         for (int i = 0; i < currentWord.getTabooWords().length; i++) {
             tabooWords.add(new ListItem(currentWord.getTabooWords()[i]));
         }
-
     }
 }
