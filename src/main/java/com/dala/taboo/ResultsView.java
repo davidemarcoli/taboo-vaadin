1806c8a441d8568050b77fd91de7fd765fdaffaa
lazarpe <lazar.petrovic.nefti@gmail.com>
2022-01-30 15:19:26 +0100

    Display winner team of the game in results page.

--- src/main/java/com/dala/taboo/ResultsView.java
+++ src/main/java/com/dala/taboo/ResultsView.java
@@ -1,7 +1,9 @@
 package com.dala.taboo;
 
 import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.html.H3;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
 
 /**
@@ -9,13 +11,26 @@ import com.vaadin.flow.router.Route;
  * Project name: taboo-vaadin
  **/
 @Route(value = "results")
-public class ResultsView extends HorizontalLayout {
+public class ResultsView extends VerticalLayout {
     H1 title = new H1();
+    H1 winnerTeam = new H1();
 
     public ResultsView() {
         title.setText("Taboo");
+        winnerTeam.setText(getWinnerTeam() + " won the game!");
         style();
-        add(title);
+        add(title, winnerTeam);
+    }
+
+    private String getWinnerTeam() {
+        for (int i = 0; i < ConfigurationService.teams.size(); i++) {
+            if (ConfigurationService.teams.get(i).getScore() > ConfigurationService.teams.get(i+1).getScore()) {
+                return ConfigurationService.teams.get(i).getTeamName();
+            } else {
+                return ConfigurationService.teams.get(i+1).getTeamName();
+            }
+        }
+        return null;
     }
 
     private void style() {
